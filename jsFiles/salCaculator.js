e135cf8b6370734b95a1420713379cf09999cdb6
Mariagoretti Kimani <mgmkimani@gmail.com>
2022-11-29 09:00:35 +0300

    challenge3 #speed

--- jsFiles/salCaculator.js
+++ jsFiles/salCaculator.js
@@ -1,3 +1,6 @@
-const prompt =require("prompt-sync")();
-function speed(){
-    //prompt user to input the speed of a c
+const prompt=require("prompt-sync")();
+function Wages(){
+    let Salary = prompt("Enter salary in Ksh.:");
+
+}
+Wages();
--- jsFiles/speedDetector.js
+++ jsFiles/speedDetector.js
@@ -9,16 +9,4 @@ function limit(){
     }
 }
 
-    // if  (speed <= 70) {
-    //     console.log('OK')
-    // }
-
-    // else if (speed > 70) {
-    //     const points = (speed - 70)/5
-
-    //     if (points >= 12)
-    //     console.log('License suspended')
-    // }
-
-
 limit();
